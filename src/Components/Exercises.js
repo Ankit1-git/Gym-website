8e7526f75c528c1a43e4dd668f1b0a8881731d3d
Ankit1-git <ankitorg15@gmail.com>
2024-03-25 19:08:49 +0530

    first commit

---
+++ src/Components/Exercises.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Pagination } from "@mui/material";
+import { Box, Typography, Stack } from "@mui/material";
+
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import ExerciseCard from "./ExerciseCard";
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisePerPage = 9;
+
+  // to show only 9 cards per page
+  const indexOfLastExercise = currentPage * exercisePerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisePerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+
+  const Paginate = (e, value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1800, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === "all") {
+        exercisesData = await fetchData(
+          "https://exercisedb.p.rapidapi.com/exercises",
+          exerciseOptions
+        );
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
+      }
+
+      setExercises(exercisesData);
+    };
+
+    fetchExercisesData();
+  }, [bodyPart]);
+
+  return (
+    <Box id="exercises" sx={{ mt: { lg: "95px" } }} mt="50px" p="20px">
+      <Typography className="style" variant="h3" mb="56px" textAlign="center">
+        Selected Results Below:
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "110px", sx: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {currentExercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisePerPage)}
+            page={currentPage}
+            onChange={Paginate}
+            size="large"
+          />
+        )}
+      </Stack>
+    </Box>
+  );
+};
+
+export default Exercises;
---
+++ src/Components/SimilarExercises.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import HorizontalScrollbar from "./HorizontalScrollbar";
+import Loader from "./Loader";
+
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
+  return (
+    <Box sx={{ mt: { lg: "100px", xs: "0" } }}>
+      <Typography className="style" variant="h4" mb={6}>
+        Exercises that target the same Muscle Group!
+      </Typography>
+      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+        {targetMuscleExercises.length ? (
+          <HorizontalScrollbar data={targetMuscleExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+      <Typography className="style" variant="h4" mb={7} mt={6}>
+        Exercises that target the same Equipments!
+      </Typography>
+      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+        {equipmentExercises.length ? (
+          <HorizontalScrollbar data={equipmentExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+    </Box>
+  );
+};
+
+export default SimilarExercises;
